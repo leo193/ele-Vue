c30ca0c99e9c804842b091b2cf716d9ac57e5857
leo_yang <yinshu6@sina.com>
2020-05-22 01:54:50 +0800

    初始化

---
+++ src/views/index.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="index">
+        主页
+    </div>
+</template>
+
+<script>
+export default {
+    name: "index" ,
+    props: {
+
+    },
+    data() {
+        return {
+
+        };
+    },
+    components: {
+
+    },
+    computed: {
+
+    },
+    created() {
+
+    },
+    mounted() {
+
+    },
+    methods: {
+
+    },
+};
+</script>
+
+<style scoped >
+
+</style>
